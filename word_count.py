65c40a74c5ad8218ef1ad0ed21b5db01d02643cc
aswin2575 <151365468+aswin2575@users.noreply.github.com>
2024-05-13 01:08:36 +0530

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -1,13 +1,24 @@
 def word_count(str1):
-    leng=len(str1[0])
+    leng=len(str1)
     count=0
+    if leng==0:
+        return count
     for i in range(leng):
-        if(i<leng-1):
-            if(str1[0][i]==" " or str1[0][i]=="."):
-                count=count+1
-    return count+1
+        if(i<leng):
+            if (str1[i]==" " or str1[i]=="."):
+                if i<leng-1:
+                    if (str1[i+1]==" " or str1[i+1]=="."):
+                        continue
+                    else:
+                        count=count+1
+                        continue
+                
+    if str1[0]==" " or str1[0]==".":
+        count=count-1               
+    if str1[leng-1] != " " or str1[leng-1] != ".":
+        count=count+1
+    return count
 #str1=['my name is aswin.I am 21 years old']
 str1=input("Enter the string:")
-print(str1)
 word=word_count(str1)
 print("Number of words in the sentence are:",word)
